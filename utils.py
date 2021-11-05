2e078c525f577a8b1c28dff6ab3f302024986353
Thomas <prog.ery4z@gmail.com>
2021-11-05 12:54:25 +0100

    Working function

--- utils.py
+++ utils.py
@@ -1,8 +1,6 @@
 from NBT.nbt.world import WorldFolder
 import random, time
 
-t_s = time.time()
-
 
 def get_chunk_pos(coord):
     c_x = coord[0] // 16
@@ -28,8 +26,14 @@ def list_chunk(coord1, coord2):
 
     chunk_list = []
     # TODO : Make this thing work for any points with the range (can only increase)
-    P0 = (min(chunk_coord1[0], chunk_coord2[0]), min(chunk_coord1[1], chunk_coord2[1]))
-    P1 = (max(chunk_coord1[0], chunk_coord2[0]), max(chunk_coord1[1], chunk_coord2[1]))
+    P0 = (
+        min(chunk_coord1[0], chunk_coord2[0]),
+        min(chunk_coord1[1], chunk_coord2[1]),
+    )
+    P1 = (
+        max(chunk_coord1[0], chunk_coord2[0]),
+        max(chunk_coord1[1], chunk_coord2[1]),
+    )
 
     for x in range(P0[0], P1[0] + 1):
         for z in range(P0[1], P1[1] + 1):
@@ -44,8 +48,14 @@ def list_chunk_region(coord1, coord2):
 
     chunk_list = []
     # TODO : Make this thing work for any points with the range (can only increase)
-    P0 = (min(chunk_coord1[0], chunk_coord2[0]), min(chunk_coord1[1], chunk_coord2[1]))
-    P1 = (max(chunk_coord1[0], chunk_coord2[0]), max(chunk_coord1[1], chunk_coord2[1]))
+    P0 = (
+        min(chunk_coord1[0], chunk_coord2[0]),
+        min(chunk_coord1[1], chunk_coord2[1]),
+    )
+    P1 = (
+        max(chunk_coord1[0], chunk_coord2[0]),
+        max(chunk_coord1[1], chunk_coord2[1]),
+    )
 
     for x in range(P0[0], P1[0] + 1):
         for z in range(P0[1], P1[1] + 1):
@@ -87,10 +97,20 @@ def map_chunks(coord1, coord2, coord3):
 
     coord4 = (coord3[0] + initial_vector[0], coord3[1] + initial_vector[1])
 
+    ref_chunk1 = get_chunk_pos(coord1)
+    ref_chunk2 = get_chunk_pos(coord3)
+
     chunk_list1 = list_chunk(coord1, coord2)
     chunk_list2 = list_chunk(coord3, coord4)
 
-    map_list = [[chunk_list1[i], chunk_list2[i]] for i in range(len(chunk_list1))]
+    map_list = []
+    for chunk1 in chunk_list1:
+        ref_vector1 = (chunk1[0] - ref_chunk1[0], chunk1[1] - ref_chunk1[1])
+        for chunk2 in chunk_list2:
+            ref_vector2 = (chunk2[0] - ref_chunk2[0], chunk2[1] - ref_chunk2[1])
+            if ref_vector1 == ref_vector2:
+                map_list.append([chunk1, chunk2])
+                break
 
     dict_region = {}
 
@@ -104,14 +124,16 @@ def map_chunks(coord1, coord2, coord3):
         if not coord_region_end in dict_region:
             dict_region[coord_region_end] = {}
 
-        dict_region[coord_region_end][normalized_chunk_coord_end].append(
-            [coord_region_start, normalized_chunk_coord_start]
-        )
+        dict_region[coord_region_end][normalized_chunk_coord_end] = [
+            coord_region_start,
+            normalized_chunk_coord_start,
+        ]
 
     return dict_region
 
 
 def copy_chunk_from_mapdict(map1, map2, map_dict):
+    t_s = time.time()
     world1 = WorldFolder(map1)
     if map1 == map2:
         world2 = world1
@@ -144,7 +166,9 @@ def copy_chunk_from_mapdict(map1, map2, map_dict):
                 start_region_key[0], start_region_key[1]
             )
         except:
-            pass
+            print(
+                f"ENTITY | Unable to open world 1 r.{start_region_key[0]}.{start_region_key[1]}"
+            )
 
     for end_region_key in necessary_region_coord_end:
         end_region[end_region_key] = world2.get_region(
@@ -155,7 +179,9 @@ def copy_chunk_from_mapdict(map1, map2, map_dict):
                 end_region_key[0], end_region_key[1]
             )
         except:
-            pass
+            print(
+                f"ENTITY | Unable to open world 2 r.{end_region_key[0]}.{end_region_key[1]}"
+            )
 
     for region_end_key in map_dict:
         end_region_object = end_region[region_end_key]
@@ -165,9 +191,11 @@ def copy_chunk_from_mapdict(map1, map2, map_dict):
 
             start_region_object = start_region[local_map[0]]
             try:
-                start_region_entities_object = start_region_entities[local_map[0]]
+                start_region_entities_object = start_region_entities[
+                    local_map[0]
+                ]
             except:
-                pass
+                print(f"ENTITY | Unable to open region {local_map[0]} of map 2")
 
             try:
                 start_chunk = start_region_object.export_chunk(
@@ -177,21 +205,45 @@ def copy_chunk_from_mapdict(map1, map2, map_dict):
                     local_end_chunk_key[0], local_end_chunk_key[1], start_chunk
                 )
             except:
-                pass
+                print(
+                    f"BLOCS | Error while exporting {local_map[1][0], local_map[1][1]}"
+                )
 
             else:
                 try:
-                    start_entities = start_region_entities_object.get_chunk_entity(
-                        local_map[1][0], local_map[1][1]
+                    start_entities = (
+                        start_region_entities_object.get_chunk_entities(
+                            local_map[1][0], local_map[1][1]
+                        )
                     )
-                    end_region_entities_object.write_chunk_entities(
-                        local_end_chunk_key[0], local_end_chunk_key[1], start_entities
+                except Exception as e:
+                    print(
+                        f"ENTITY | Error while loading entity {local_map[1]} : {e}"
                     )
-                except Exception:
-                    pass
+                else:
+                    try:
+                        end_region_entities_object.write_chunk_entities(
+                            local_end_chunk_key[0],
+                            local_end_chunk_key[1],
+                            start_entities,
+                        )
+                    except Exception:
+                        print(
+                            f"ENTITY | Error while writing entity {local_end_chunk_key}"
+                        )
+    print(time.time() - t_s)
 
-        # do stuff with the chunk
-        # if random.random() > 0.5:
+    # do stuff with the chunk
+    # if random.random() > 0.5:
 
 
-print(time.time() - t_s)
+map1 = "C:\\Users\\Thomas\\AppData\\Roaming\\.minecraft\\saves\\New World (1)"
+map2 = map1
+
+coord1 = (598, 500)
+coord2 = (637, 418)
+coord3 = (777, 375)
+
+map_dict = map_chunks(coord1, coord2, coord3)
+
+copy_chunk_from_mapdict(map1, map2, map_dict)
